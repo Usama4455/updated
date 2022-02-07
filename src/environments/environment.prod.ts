610316b447261e20605cd064841e18e8c530bbbf
Usama4455 <usamasatti200@gmail.com>
2022-02-07 11:51:21 +0500

    Enter

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,27 @@
+export const environment = {
+  production: true,
+  sync: true,
+  //baseUrl: 'https://root.sharre.pk/my_life/prod',
+  baseUrl: 'https://root.sharre.pk/my_life/staging',
+  verifyUrl: 'https://verify.cliq.pk',
+  consumerKey: 'ck_fbca6787a4ffd73829a36f0c9628f5bdfa677d84',
+  consumerSecret: 'cs_11f5cf48b9e0216e2463b7e2329df7f6df3009cc',
+  firebase : {
+    apiKey: "AIzaSyAC3_wmveZzuXIdbhpZqUHkHj52_50Jad4",
+    authDomain: "usama-764a0.firebaseapp.com",
+    databaseURL: "https://cliq-20ef3.firebaseio.com",
+    projectId: "usama-764a0",
+    storageBucket: "usama-764a0.appspot.com",
+    messagingSenderId: "314742191189",
+    appId: "1:314742191189:web:d487ab92dd60ca1c6dd0dc",
+    measurementId: "G-L6C0YR2G28"
+    //apiKey: "AIzaSyACddiXh7FAw2MhP2YV337WdPAjmN0sCNE",
+   // authDomain: "cliq-20ef3.firebaseapp.com",
+    //databaseURL: "https://cliq-20ef3.firebaseio.com",
+    //projectId: "cliq-20ef3",
+    //storageBucket: "cliq-20ef3.appspot.com",
+    //messagingSenderId: "116848056554",
+    //appId: "1:116848056554:web:75c36958c4f5afa047df93",
+    //measurementId: "G-7TFLPHE93W"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,39 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  sync: false,
+  baseUrl: 'https://root.sharre.pk/my_life/staging',
+  verifyUrl: 'https://verify.cliq.pk',
+  consumerKey: 'ck_fbca6787a4ffd73829a36f0c9628f5bdfa677d84',
+  consumerSecret: 'cs_11f5cf48b9e0216e2463b7e2329df7f6df3009cc',
+  firebase : {
+    apiKey: "AIzaSyAC3_wmveZzuXIdbhpZqUHkHj52_50Jad4",
+    authDomain: "usama-764a0.firebaseapp.com",
+    databaseURL: "https://cliq-20ef3.firebaseio.com",
+    projectId: "usama-764a0",
+    storageBucket: "usama-764a0.appspot.com",
+    messagingSenderId: "314742191189",
+    appId: "1:314742191189:web:d487ab92dd60ca1c6dd0dc",
+    measurementId: "G-L6C0YR2G28"
+    //apiKey: "AIzaSyACddiXh7FAw2MhP2YV337WdPAjmN0sCNE",
+   // authDomain: "cliq-20ef3.firebaseapp.com",
+   // databaseURL: "https://cliq-20ef3.firebaseio.com",
+    //projectId: "cliq-20ef3",
+    //storageBucket: "cliq-20ef3.appspot.com",
+    //messagingSenderId: "116848056554",
+    //appId: "1:116848056554:web:75c36958c4f5afa047df93",
+    //measurementId: "G-7TFLPHE93W"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+//import 'zone.js/dist/zone-error';  // Included with Angular CLI.
